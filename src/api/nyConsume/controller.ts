a2af9fa9164df38e1dc9785d67a5a3c95f78bf82
Zhandor Lehandro Gadêlha de Carvalho Gonzaga <zhandor.gonzaga@pampa.compasso>
2021-01-08 11:42:54 -0300

    remocao da dependencia Superagent e inclusao e implementação do uso da Axios

--- src/api/nyConsume/controller.ts
+++ src/api/nyConsume/controller.ts
@@ -1,27 +1,35 @@
 import {Request, Response, NextFunction } from 'express'
-import superagent from 'superagent'
+import axios from 'axios'
 
 import {extAPI} from '../../shared/environment/environment'
 
 const science = (req: Request, res: Response, next: NextFunction) => {
-	const retorno = superagent.get(extAPI.host+'topstories/v2/science.json?api-key='+extAPI.appKey).end((err, internalResponse) => {
-		if(err){
-			console.log({message: err})
-			res.status(401).json({message: err})
-		}else{
-			res.status(200).send(internalResponse.text)
+	axios({
+		method: 'get',
+		baseURL: extAPI.host,
+		url: 'topstories/v2/science.json',
+		params: {
+			'api-key': extAPI.appKey
 		}
+	}).then((response) => {
+		res.status(200).json({result: response.data.results})
+	}).catch((error) => {
+		res.status(401).send({error})
 	})
 }
 
 const technology = (req: Request, res: Response, next: NextFunction) => {
-	const retorno = superagent.get(extAPI.host+'topstories/v2/technology.json?api-key='+extAPI.appKey).end((err, internalResponse) => {
-		if(err){
-			console.log({message: err})
-			res.status(401).json({message: err})
-		}else{
-			res.status(200).send(internalResponse.text)
+	axios({
+		method: 'get',
+		baseURL: extAPI.host,
+		url: 'topstories/v2/technology.json',
+		params: {
+			'api-key': extAPI.appKey
 		}
+	}).then((response) => {
+		res.status(200).json({result: response.data.results})
+	}).catch((error) => {
+		res.status(401).send({error})
 	})
 }
 
